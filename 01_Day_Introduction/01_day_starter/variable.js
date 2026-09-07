70e3e2ca5a96c383f216f80ac30b876d8c943a8d
chow <zgy1299408744@163.com>
2026-09-07 18:25:01 +0800

    fix: day2 typo

--- 01_Day_Introduction/01_day_starter/variable.js
+++ 01_Day_Introduction/01_day_starter/variable.js
@@ -13,7 +13,7 @@ const gravity = 9.81     // Earth's gravity in m/s^2
 const boilingPoint = 100 // boiling point of water in °C
 const PI = 3.14          // mathematical constant
 
-// Variables can also be declared in across multiple lines, separated by commas
+// Variables can also be declared across multiple lines, separated by commas
 
 let name = 'Asabeneh', // name of a person
   job = 'teacher',
