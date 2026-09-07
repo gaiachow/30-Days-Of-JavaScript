7d4600722e831d571ba473576cb6450632e3f7fb
chow <zgy1299408744@163.com>
2026-09-07 18:24:36 +0800

    fix: day2 typo

--- 01_Day_Introduction/01_day_starter/variable.js
+++ 01_Day_Introduction/01_day_starter/variable.js
@@ -7,13 +7,13 @@ let city = 'Helsinki'      // capital
 let age = 100              // age in years
 let isMarried = true
 
-// Declaring variables with number values
+// Declare variables with number values
 
 const gravity = 9.81     // Earth's gravity in m/s^2
 const boilingPoint = 100 // boiling point of water in °C
 const PI = 3.14          // mathematical constant
 
-// Variables can also be declared in one line, separated by commas
+// Variables can also be declared in across multiple lines, separated by commas
 
 let name = 'Asabeneh', // name of a person
   job = 'teacher',
