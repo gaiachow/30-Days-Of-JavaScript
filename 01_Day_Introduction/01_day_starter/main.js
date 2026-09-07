b5a4702b33ba51b7e466c9e7d326d17c870622ec
chow <zgy1299408744@163.com>
2026-09-07 13:28:55 +0800

    fix: day2 typo

--- 01_Day_Introduction/01_day_starter/main.js
+++ 01_Day_Introduction/01_day_starter/main.js
@@ -1,4 +1,4 @@
-// The variable values can be accessed from the variable.js file
+// These variables are defined in variable.js
 console.log(firstName, lastName, country, city, age, isMarried) 
 console.log(gravity, boilingPoint, PI) // 9.81, 100, 3.14
 console.log(name, job, residence)
