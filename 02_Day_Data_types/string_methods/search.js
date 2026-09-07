320429df6c027fcf88e9a88dcb9c07ec861704e6
chow <zgy1299408744@163.com>
2026-09-07 12:58:34 +0800

    fix: day2 typo

--- 02_Day_Data_types/string_methods/search.js
+++ 02_Day_Data_types/string_methods/search.js
@@ -6,5 +6,5 @@
   string.search(substring)
 */
 
-let string = 'I love JavaScript. If you do not love JavaScript what else can you love.'
+let string = 'I love JavaScript. If you do not love JavaScript, what else can you love?'
 console.log(string.search('love')) // 2
